a1bc4dd154ef46aff4d042d7b4a5457853d95e38
CoyoteBringsFire <aumkara@gmail.com>
2015-10-16 22:29:37 -0700

    implement verifying first argument is a file string

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 "use strict";
+var Q 	= require('q'),
+	fs 	= require('fs');
 
 module.exports 	= {
-	db2json: function converttoJson() {},
+	db2json: function converttoJson(file) {
+		var conversionPromise = Q.defer();
+
+		if( file instanceof String ) {
+			fs.readFile(file, function inputFileRead(err, data) {
+				if( err ) {
+					conversionPromise.reject(err);
+				} else {
+					// if an error happens while processing the file, reject the promise
+					conversionPromise.resolve(data);
+				}
+			});
+			return conversionPromise.promise;
+		} else {
+			return null;
+		}
+	},
 	json2db: function convertToDb() {}
 };
