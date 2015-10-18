cbd2f9603d13cf014ef466d3a68ca7a71c01277f
CoyoteBringsFire <aumkara@gmail.com>
2015-10-17 22:03:25 -0700

    added test for error reading file

--- test/db2json.js
+++ test/db2json.js
@@ -1,7 +1,8 @@
 "use strict";
 
 var should 	= require('should'),
-	bunyan 	= require('bunyan');
+	bunyan 	= require('bunyan'),
+	rewire 	= require('rewire');
 
 var logFile = __dirname + '/logs/db2json.log';
 
@@ -23,15 +24,31 @@ describe('db2json', function constructorTest() {
 		should( d2j({ file:"data/world.db" }) ).not.be.ok();
 	});
 	it('should return a thennable', function doIt(done) { 
-		var d2j = require('../index.js').db2json;
 		this.timeout(10000);
-		d2j("data/simple.db").
-			then( function onJsonReady(world) {
+		var d2j = require('../index.js').db2json;
+		d2j("data/simple.db")
+			.then( function onJsonReady(world) {
 				world.should.be.ok();
 				done();
 			});
 	});
-	it('should reject the returned promise if an error occurs while parsing the file', function doIt() { should.fail(); });
+	it('should reject the returned promise if an error occurs reading the file', function doIt(done) { 
+		this.timeout(10000);
+		var parser = rewire('../index.js');
+		parser.__set__('fs', {
+			readFile: function(filename, callback) {
+				callback(new Error("TESTERROR"));
+			}
+		});
+		parser.db2json("data/simple.db")
+			.then( function onJsonReady(world) {
+				should.fail();
+			}, function onReject(err) {
+				err.should.be.ok();
+				err.message.should.equal("TESTERROR");
+				done();
+			});
+	});
 	describe('object', function objectTest() {
 		describe('intro block', function introBlockTest() {
 			it('should contain total number of objects', function doIt() { should.fail(); });
